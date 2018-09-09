10cc2a19b97193153a2c4b124f0787dc45ccdf0c
Bernhard Johannessen <bernhard@voytsje.com>
2018-09-10 00:23:29 +0100

    Add basic wine environment settingAdd basic wine environment settingss

---
+++ wine32-env.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+# Wine 32-bit environment
+#
+# Put this file into your wine prefix directory.
+# You can then quickly set your wine environment by sourcing this file
+
+DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
+
+export WINEPREFIX="${DIR}"
+export WINEARCH="win32"
+
